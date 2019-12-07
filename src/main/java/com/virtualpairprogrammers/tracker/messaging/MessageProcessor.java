75bd43849a6054e5140e01d72157d43109bec87c
Ketan Tambe <ketan@ketan-PC>
2019-12-07 23:56:00 +0530

    Fixing Bad Build

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -33,7 +33,7 @@ public class MessageProcessor {
 				                          .withLat(new BigDecimal(incomingMessage.get("lat")))
 				                          .withLng(new BigDecimal(incomingMessage.get("long")))
 				                          .withTimestamp(convertedDatestamp)
-										  .withSpeed(47.7)
+										  .withSpeed(new BigDecimal("47.7"))
 				                          .build();
 				                          
 		data.updatePosition(newReport);
